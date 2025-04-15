e368270ea189b75d723baf7795589326b91de765
Camy Kam <60831407+CamyKam@users.noreply.github.com>
2025-04-15 13:59:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ var access_token;
 var searchResults;
 
 function loadClient() {
-  gapi.client.setApiKey("AIzaSyA--pIkKHQh4liTYnEJnRxFx_8HiZOQ-qM");
+  gapi.client.setApiKey("INSERT API KEY HERE");
   return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
       .then(function() { console.log("GAPI client loaded for API"); },
             function(err) { console.error("Error loading GAPI client for API", err); });
